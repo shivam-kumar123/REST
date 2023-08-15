7f6986f53f3709c3614e0dfca6754a1517211bde
shivam kumar <20051252@kiit.ac.in>
2023-08-15 13:00:33 +0530

    adding middlewares, for validations in RESTful API'S

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ const PORT = 3001;
 const userRoute = require('./routes/User.js');
 const commentRoute = require('./routes/Comments.js');
 
-app.use('/user', userRoute);
-app.use('/comment', commentRoute);
+// middleware
+app.use( express.json() );
+
+// middleware
+const checkIfHacker = (req, res, next) => {
+    try{    
+        const name = req.body.name;
+        if(name !== 'hacker') {
+            next();
+        } else {
+            res.status(200).send({ error: 'hackers are not allowed' });
+        }
+    } catch (e) {
+        res.status(500).send({ error: 'internal server error' });
+    }
+};
+
+app.use('/user', checkIfHacker, userRoute);
+app.use('/comment', checkIfHacker, commentRoute);
 
 app.listen(PORT, () => {
     console.log(`server running on port: ${PORT}`);
